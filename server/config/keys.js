24cdb8e0720b7edd3ac12d0c46b8796f26d522e6
Shir Dahan <46330876+Dshir32@users.noreply.github.com>
2021-08-29 09:10:50 +0300

    Delete keys.js

--- server/config/keys.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    googleClientID : "1046253482581-qirdi52388nkfpe4t33q2c2t1fe6egm6.apps.googleusercontent.com",
-    googleClientSecret : "t3RHPRf_jNAk-SAYzclYyAG7",
-    cookieKey: "googletoshir"
-}
